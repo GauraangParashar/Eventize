d222fac79245ee058681dc8e181155bb27e3c8e0
Gauraang Parashar <gauraangparashar@gmail.com>
2021-05-14 02:41:19 +0530

    routing Event Feed

--- src/containers/_nav.js
+++ src/containers/_nav.js
@@ -2,7 +2,7 @@ import React from "react";
 import CIcon from "@coreui/icons-react";
 
 const _nav = [
-  {
+  /* {
     _tag: "CSidebarNavItem",
     name: "Dashboard",
     to: "/dashboard",
@@ -11,11 +11,11 @@ const _nav = [
       color: "info",
       text: "NEW",
     },
-  },
+  }, */
   {
     _tag: "CSidebarNavItem",
-    name: "Feed",
-    to: "/feed",
+    name: "Events",
+    to: "/",
     icon: <CIcon name="cil-speedometer" customClasses="c-sidebar-nav-icon" />,
     badge: {
       color: "info",
--- src/routes.js
+++ src/routes.js
@@ -72,14 +72,14 @@ const ParticipantRegister = React.lazy(() =>
 );
 
 const routes = [
-  { path: "/", exact: true, name: "Home" },
-  { path: "/dashboard", name: "Dashboard", component: Dashboard },
   {
-    path: "/feed",
+    path: "/",
     exact: true,
-    name: "feed",
+    name: "Events",
     component: eventpage,
   },
+  /*   { path: "/dashboard", name: "Dashboard", component: Dashboard },
+   */
   /* { path: "/theme", name: "Theme", component: Colors, exact: true },
   { path: "/theme/colors", name: "Colors", component: Colors },
   { path: "/theme/typography", name: "Typography", component: Typography },
